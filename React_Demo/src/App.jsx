079ed99987432efe9350cac7231c3603a2fe060e
Ganesh Rathod <ganeshir1234@gmail.com>
2026-01-23 22:28:48 +0530

    Fixed React_Demo nested git issue

---
+++ React_Demo/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+  <div id="parent">
+    <h1 id="child1"> Gannu </h1>
+    <h2 id="child2"> Rathod  </h2>
+  </div>
+  <div> Hello </div>
+  </>
+  )
+};
+
+export default App;

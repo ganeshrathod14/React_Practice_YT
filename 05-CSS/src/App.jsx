f3690368e4e38eb21f3baa2d5beea1ead93541b2
Ganesh Rathod <ganeshir1234@gmail.com>
2026-01-27 19:44:35 +0530

    New Changes

---
+++ 05-CSS/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from './Components/Button/Button'
+import Header from './Components/header/Header'
+
+
+const App = () => {
+  return (
+    <div>
+        <Button/>
+        <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ 05-CSS/src/Components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={style.header}>
+      Itna na mujhase tu pyar badha
+      <button className="btn"> button </button>
+    </div>
+  );
+};
+
+export default Header;

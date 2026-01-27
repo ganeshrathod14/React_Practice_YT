513162f40d2222d3953179aad29530337c9bd30a
Ganesh Rathod <ganeshir1234@gmail.com>
2026-01-27 22:46:32 +0530

    Tailwing Litle bit Change

---
+++ 06-Tailwind/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div> 
+     <h2 className='max-h-screen max-w-max bg-amber-600'> Rathod </h2>  
+      </div>
+  )
+}
+
+export default App
